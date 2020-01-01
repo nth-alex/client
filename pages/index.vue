6966cd1e544a9c4e723f00aaf10d3ec6ceaec5d3
Alexander <nth.alexx@gmail.com>
2020-01-02 00:31:13 +0200

    first init

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container">
+        <div><i class="fa fa-smile-o"></i></div>
+        <p>this is index page</p>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
