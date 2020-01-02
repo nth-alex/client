82f4636a40bc97da86ca9749098e494336fa39b1
Alexander <nth.alexx@gmail.com>
2020-01-02 15:48:18 +0200

    update

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+        this is about page
+        <hr>
+        <nuxt-link to="/">home</nuxt-link>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,8 @@
     <div class="container">
         <div><i class="fa fa-smile-o"></i></div>
         <p>this is index page</p>
+        <hr>
+        <nuxt-link to="/about">about</nuxt-link>
     </div>
 </template>
 
